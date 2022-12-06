fd95d675a0735696844234101bd53fd067bb8fcf
Napoleon Farfetch <nfarfetc@et-l6.kzn.21-school.ru>
2022-12-06 16:07:40 +0300

    Initial some handlers

---
+++ cmd/web/handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Salam from SnippetBox!"))
+}
+
+func showSnippets(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Snippet with ID %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "This Method is forbidden!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Form for creating a new snippet!"))
+}
