64273433c4f30d03803fae9022c9915b36f9d7fe
Napoleon Farfetch <nfarfetc@et-l6.kzn.21-school.ru>
2022-12-07 16:28:48 +0300

    Add dabase connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -16,20 +20,38 @@ type application struct {
 type Config struct {
 	Addr      string
 	StaticDir string
+	DBHost    string
+	DBUser    string
+	DBPort    string
+	DBName    string
+	Pass      string
 }
 
 func main() {
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":80", "Server Address")
 	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Directory with static files")
+	// flag.StringVar(&cfg.DSN, "dsn", "web:1111@localhost:5432/SnippetBox", "Database source data")
+	flag.StringVar(&cfg.DBHost, "host", "localhost", "The Hosy to connect to")
+	flag.StringVar(&cfg.DBPort, "port", "5432", "The Port to bind to")
+	flag.StringVar(&cfg.DBUser, "user", "web", "The user to sign in as")
+	flag.StringVar(&cfg.Pass, "password", "1111", "The user's password")
+	flag.StringVar(&cfg.DBName, "dbname", "SnippetBox", "The name of database to connect to")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "\u001b[36m[INFO]\u001b[0m\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "\u001b[31m[ERROR]\u001b[0m\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.Pass, cfg.DBName))
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
-		infoLog: infoLog,
+		infoLog:  infoLog,
 	}
 
 	srv := &http.Server{
@@ -39,8 +61,19 @@ func main() {
 	}
 
 	infoLog.Printf("Server Start! at %s", cfg.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 type neuteredFileSystem struct {
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module snippetbox
 
 go 1.18
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
