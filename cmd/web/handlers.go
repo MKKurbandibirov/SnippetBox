5d75ecf83591ecc0cf28c20c158beb4a2c02cb58
Napoleon Farfetch <nfarfetc@et-l6.kzn.21-school.ru>
2022-12-12 12:24:11 +0300

    Complete db query imple

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"snippetbox/pkg/models"
 	"strconv"
-	"text/template"
+	//"text/template"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -13,22 +15,32 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err)
+	for _, s := range s {
+		fmt.Fprintf(w, "%v\n", s)
 	}
+
+	// files := []string{
+	// 	"./ui/html/home.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.Execute(w, nil)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
 }
 
 func (app *application) showSnippets(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +50,17 @@ func (app *application) showSnippets(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Snippet with ID %d", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- pkg/models/postgresql/snippets.go
+++ pkg/models/postgresql/snippets.go
@@ -2,11 +2,31 @@ package postgresql
 
 import (
 	"database/sql"
+	"errors"
 	"snippetbox/pkg/models"
 )
 
 type SnippetModel struct {
 	DB *sql.DB
+}
+
+func (sm *SnippetModel) Get(id int) (*models.Snippet, error) {
+	stmt := `SELECT id, title, content, created, expired FROM snippets WHERE expired > now() AND id = $1`
+
+	row := sm.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expired)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
 }
 
 func (sm *SnippetModel) Insert(title, content string, expired int) (int, error) {
@@ -21,10 +41,32 @@ func (sm *SnippetModel) Insert(title, content string, expired int) (int, error) 
 	return int(id), nil
 }
 
-func (sm *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
-}
-
 func (sm *SnippetModel) Latest() ([]*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expired FROM snippets WHERE snippets.expired > now() ORDER BY created DESC LIMIT 10`
+
+	rows, err := sm.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var snippets []*models.Snippet
+
+	for rows.Next() {
+		s := &models.Snippet{}
+
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expired)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
